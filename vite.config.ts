795b1d0e4dfe426c8046450ca7b558dbcb2cde56
Nite <kylerjmancos@gmail.com>
2025-08-01 19:19:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Geopolitics/", // exactly matches your repo name
+  plugins: [react()],
+});
+
+export default defineConfig({
   plugins: [react({ jsxRuntime: 'classic' })],
   base: '/Geopolitics/',
 });
