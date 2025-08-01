38882a8d3322f3ce4161091b3ae0f92be32b7f82
Nite <kylerjmancos@gmail.com>
2025-08-01 23:43:45 +0000

    Fix config and remove merge conflict markers

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/Geopolitics/", // exactly matches your repo name
-  plugins: [react()],
-});
-
-export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' })],
   base: '/Geopolitics/',
-=======
-  base: '/Geopolitics/', // important for GitHub Pages
-  plugins: [react()],
->>>>>>> 3653a40 (Fix default export)
+  plugins: [react({ jsxRuntime: 'classic' })],
 });
-
